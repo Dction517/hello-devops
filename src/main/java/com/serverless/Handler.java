cadcbd2bdce05a34723b95b8850df1b3bde12101
dction <yucheng_lin@hotmail.com>
2021-03-21 22:50:57 +0800

    add an error code

--- src/main/java/com/serverless/Handler.java
+++ src/main/java/com/serverless/Handler.java
@@ -17,6 +17,7 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
 	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
 		LOG.info("received: {}", input);
 		Response responseBody = new Response("Hello DevOps!", input);
+		I am an error
 		return ApiGatewayResponse.builder()
 				.setStatusCode(200)
 				.setObjectBody(responseBody)
